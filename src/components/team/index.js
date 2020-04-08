9f5b574be4f89da589e79a1912e3a6903ac33cd9
KJ Wilson <yourboykjdev@gmail.com>
2020-04-08 06:38:44 -0700

    added photos to team

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,27 +1,29 @@
-import React, {Component} from "react"
-import ceo from '../../ceo.jpg';
+import React, { Component } from "react";
+import ceo from "../../ceo.jpg";
+import kid from "../../dnakid.jpg";
+import kids from "../../dnashow.jpg";
 
 class Team extends Component {
-    render() {
-        return (
-            <div className="App">
-              <header className="App-header">
-                <img src={ceo} className="App-logo" alt="ceo" />
-                <p>
-                  ceo
-                </p>
-                <a
-                  className="App-link"
-                  href="https://www.makeschool.com/portfolio/kjwilson"
-                  target="_blank" // opens link in new tab
-                  rel="noopener noreferrer"
-                >
-                  portfolio
-                </a>
-              </header>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ceo} className="App-logo" alt="ceo" />
+          <p>ceo</p>
+          <img src={kid} className="App-logo" alt="kid" />
+          <img src={kids} className="App-logo" alt="kids" />
+          <a
+            className="App-link"
+            href="https://www.makeschool.com/portfolio/kjwilson"
+            target="_blank" // opens link in new tab
+            rel="noopener noreferrer"
+          >
+            portfolio
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default Team
+export default Team;
