12dc28683790cc92a0a8a4e50ef1a1a7f9ccc15e
KJ Wilson <yourboykjdev@gmail.com>
2020-04-08 00:41:41 -0700

    refactor exp button

--- src/components/experienceButton/index.js
+++ src/components/experienceButton/index.js
@@ -9,71 +9,120 @@ class ExperienceButton extends Component {
     super(props);
     this.state = {
       showIcons: false,
+      firstClick: false,
+      onClickBrandCalled: false,
     };
   }
 
+  onClickBrand(e) {
+    console.log("CLICKED");
+    if (this.state.firstClick && !this.state.onClickBrandCalled) {
+      console.log("FIRST CLICK: DONT OPEN");
+      console.log(e.target.tagName);
+      e.preventDefault();
+      this.setState({ firstClick: false, onClickBrandCalled: true });
+    } else {
+      console.log("NOT FIRST CLICK, please open");
+    }
+  }
+
   render() {
+    console.log("RERENDERING");
     const { instagram, soundcloud, hyperfollow } = this.props;
 
-    let instagramButton = "";
-    let soundcloudButton = "";
-    let hyperfollowButton = "";
-
-    if (instagram) {
-      instagramButton = (
-        <a target="_blank" href={instagram}>
-          <FontAwesomeIcon className="ico" icon={faInstagram} size="2x" />
-        </a>
-      );
-    }
-
-    if (soundcloud) {
-      soundcloudButton = (
-        <a target="_blank" href={soundcloud}>
-          <FontAwesomeIcon className="ico" icon={faSoundcloud} size="2x" />
-        </a>
-      );
-    }
-
-    if (hyperfollow) {
-      hyperfollowButton = (
-        <a target="_blank" href={hyperfollow}>
-          <FontAwesomeIcon className="ico" icon={faMusic} size="2x" />
-        </a>
-      );
-    }
-
-    // let content = "EXPERIENCE";
-    // if (this.state.showIcons) {
-    //   content = (
-    // <ul className="icons">
-    //   <li>i</li>
-    //   <li>s</li>
-    //   <li>f</li>
-    // </ul>
-    //   );
-    // }
-
-    console.log(this.state.showIcons);
-    return (
-      <div
-        className="expbutton"
-        onMouseOver={() => this.setState({ showIcons: true })}
-        onMouseOut={() => this.setState({ showIcons: false })}
-        onFocus={() => this.setState({ showIcons: !this.state.showIcons })}
-        onBlur={() => this.setState({ showIcons: false })}
-      >
-        {this.state.showIcons ? (
-          <ul className="icons">
-            <li>{instagramButton}</li>
-            <li>{soundcloudButton}</li>
-            <li>{hyperfollow}</li>
-          </ul>
-        ) : (
-          "EXPERIENCE"
-        )}
-      </div>
+    console.log(
+      `showIcons: ${this.state.showIcons}, firstClick: ${this.state.firstClick}, onClickBrandCalled: ${this.state.onClickBrandCalled}`
     );
+
+    // NOTE: onMouseOver and onMouseOut default to clicks for mobile, and hover on web
+    if (!this.state.showIcons) {
+      return (
+        <div
+          className="expbutton"
+          onMouseOver={(e) => {
+            console.log("PARENT ON MOUSE OVER CALLED");
+            if (!this.state.showIcons) {
+              this.setState({ showIcons: true, firstClick: true });
+            }
+          }}
+          // tabIndex={0}
+        >
+          <p>EXPERIENCE</p>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="expbutton"
+          // onMouseOver={(e) => console.log(e.target.tagName)}
+          onMouseOut={(e) => {
+            console.log("CHILD ON MOUSE OUT CALLED");
+            console.log(e.target.tagName, typeof e.target.tagName);
+            if (e.target.tagName !== "svg" && e.target.tagName !== "path") {
+              // Handles edge case: button closes when svg tag is clicked
+              this.setState({
+                showIcons: false,
+                firstClick: false,
+                onClickBrandCalled: false,
+              });
+            }
+          }}
+          onBlur={() =>
+            this.setState({
+              showIcons: false,
+              firstClick: false,
+              onClickBrandCalled: false,
+            })
+          }
+          // tabIndex={0}
+        >
+          {/* NOTE: These tabs will need a double click to work */}
+          <ul className="icons">
+            {instagram ? (
+              <li>
+                <a
+                  target="_blank"
+                  href={instagram}
+                  onClick={(e) => this.onClickBrand(e)}
+                >
+                  <FontAwesomeIcon
+                    className="ico"
+                    icon={faInstagram}
+                    size="2x"
+                  />
+                </a>
+              </li>
+            ) : null}
+            {soundcloud ? (
+              <li>
+                <a
+                  target="_blank"
+                  href={soundcloud}
+                  onClick={(e) => this.onClickBrand(e)}
+                >
+                  <FontAwesomeIcon
+                    className="ico"
+                    icon={faSoundcloud}
+                    size="2x"
+                  />
+                </a>
+              </li>
+            ) : null}
+            {hyperfollow ? (
+              <li>
+                <a
+                  target="_blank"
+                  href={hyperfollow}
+                  onClick={(e) => this.onClickBrand(e)}
+                >
+                  <FontAwesomeIcon className="ico" icon={faMusic} size="2x" />
+                </a>
+              </li>
+            ) : null}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
